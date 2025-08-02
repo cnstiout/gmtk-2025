f8bd568ae83123c8971cb3f3dce5ddcbcbafc77a
Gabinary <gabigabdu72@hotmail.com>
2025-08-02 14:09:12 +0200

    Added score ui animation and changed start boost sound

--- scripts/ui/hud.gd
+++ scripts/ui/hud.gd
@@ -6,14 +6,16 @@ extends CanvasLayer
 @onready var radar_noise: AudioStreamPlayer = $RadarNoise
 @onready var lives: BoxContainer = %Lives
 
-var max_health: int
+var max_health: int 
+var current_score: int = 0
 
 func _ready() -> void:
 	Events.player_speed_changed.connect(change_speed)
-	Events.new_run_score.connect(change_run_score)
+	Events.new_run_score.connect(change_run_score_animate)
 	Events.player_health_changed.connect(change_health)
 
 func reset(starting_speed: int) -> void:
+	current_score = 0
 	speed_label.text = str(starting_speed)
 	score_label.text = "0"
 	set_health(max_health)
@@ -22,8 +24,12 @@ func change_speed(speed: int) -> void:
 	speed_label.text = str(speed)
 
 func change_run_score(score: int) -> void:
-	score_label.text = str(score)
+	score_label.text = str(round(score))
+
+func change_run_score_animate(score: int) -> void:
 	radar_noise.play()
+	var tween: Tween = get_tree().create_tween()
+	tween.tween_method(change_run_score, current_score, score, 0.8).set_trans(Tween.TRANS_EXPO)
 
 func set_health(value: int) -> void:
 	var lives_array: Array[Node] = lives.get_children()
