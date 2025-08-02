f31267a7f852ccce4a43fa3a823531fa2c03500e
Gabinary <gabigabdu72@hotmail.com>
2025-08-02 22:45:53 +0200

    Changed button hover implementation

--- scripts/ui/texture_button.gd
+++ scripts/ui/texture_button.gd
@@ -2,6 +2,10 @@ extends TextureButton
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
+func _ready() -> void:
+	mouse_entered.connect(animate_hover)
+	mouse_exited.connect(animate_hover_exit)
+
 func animate_hover() -> void:
 	animation_player.play("hover")
 
